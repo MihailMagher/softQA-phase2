e49c8f17ae37de8d09655dc3a125732a988c87f6
MihailMagher <91478244+MihailMagher@users.noreply.github.com>
2025-02-07 13:29:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 #start to the terminal code
+import os
+import sys
+import time
+import subprocess
+import platform
+import getpass
+import shutil
+import requests
+import json
+import re
+
+while True:
+    #ask for the action
+    act = input("enter action:")
+    if( act == "help"):
+        print("--------------------")
+        print("help: show all commands")
+        print("login start login system")
+        print("exit exit the system")
+        print("--------------------")

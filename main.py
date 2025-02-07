c9276a40f5c2aa65d1ddcec3129a36fa2b613a33
MihailMagher <91478244+MihailMagher@users.noreply.github.com>
2025-02-07 13:49:56 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,13 +9,28 @@ import shutil
 import requests
 import json
 import re
+import login
 
 while True:
     #ask for the action
-    act = input("enter action:")
-    if( act == "help"):
-        print("--------------------")
+    act = input("enter action, type help for more information:")
+
+    #help command gives explenation of commands and program
+    if(act == "help"):
+        print("\n\n--------------------")
+        print("<COMMANDS> \n")
         print("help: show all commands")
-        print("login start login system")
-        print("exit exit the system")
+        print("login: start login system")
+        print("exit: exit the system")
         print("--------------------")
+        print("<DESCRIPTION> \n")
+        print("this is a financian banking system, you can withdraw money, depisite money, and all the other things a banking app does all from the comfor of your terminal. \n\nTo log in just type 'login' to begin the login process. ")
+        print("--------------------\n\n")
+
+    #exit command exits the program
+    elif(act == "exit"):
+        break
+
+    #login command starts the login system
+    elif(act == "login"):
+        login.login()

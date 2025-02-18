a4dff4d66a925d55edfae44fa47977905e278960
MihailMagher <91478244+MihailMagher@users.noreply.github.com>
2025-02-17 22:18:30 -0500

    Add files via upload
    modified session to include withdraw and transfer

--- BigBank/session.py
+++ BigBank/session.py
@@ -1,23 +1,17 @@
 import getpass
 import os
 from account import Accounts
-from colorama import Fore, Style
 
 class Session:
     def __init__(self, users, accounts_file, bank):
         self.users = users
         self.accounts_file = accounts_file
-        self.bank = bank  
-        self.logged_in_user = None  
-        self.newly_created_accounts = []
+        self.bank = bank 
+        self.logged_in_user = None
 
     def login(self):
-       
         while True:
             self.clear_screen()
-       
-
-
             print("Welcome to the Bank Console Application!")
             print("Select session type:")
             print("[1] Standard Session")
@@ -33,75 +27,57 @@ class Session:
                 print("Exiting application...")
                 exit()
             else:
-                print(Fore.RED + "Invalid selection. Please choose 1, 2, or 0." + Style.RESET_ALL)
+                print("Invalid selection. Please choose 1, 2, or 0.")
 
     def standard_login(self):
-        
         input_name = input("Enter your Name: ").strip().replace("_", " ")  
         
+        # Find matching user (not an admin)
         found_user = next((user for user in self.users.values() if user.name.lower() == input_name.lower() and not user.is_admin()), None)
 
         if not found_user:
-            print(Fore.RED + "Invalid Name. Please try again." + Style.RESET_ALL)
+            print("Invalid Name. Please try again.")
             return
 
-        self.authenticate(found_user)  
+        self.authenticate(found_user) 
+
 
     def admin_login(self):
         input_name = input("Enter your Admin Name: ").strip().replace("_", " ")
 
-        found_user = next((user for user in self.users.values() if user.name.lower() == input_name.lower() and user.is_admin()), None)
+        found_user = None
+        for user in self.users.values():
+            if user.name.lower() == input_name.lower() and user.is_admin():
+                found_user = user
+                break
 
-        if not found_user:
-            print(Fore.RED + "Invalid Admin Name. Please try again." + Style.RESET_ALL)
-            return
-
-        self.authenticate(found_user)
+        if found_user:
+            self.authenticate(found_user)
+        else:
+            print("Invalid Admin Name. Please try again.")
 
     def authenticate(self, user):
-        password = getpass.getpass("Enter your password: ")
+        password = getpass.getpass("Enter your password: ") 
         if password != user.default_password:
-            print(Fore.RED + "Invalid password. Please try again." + Style.RESET_ALL)
+            print("Invalid password. Please try again.")
             return
 
         if not user.is_active():
-            print(Fore.RED + "Access Denied: Your account is disabled." + Style.RESET_ALL)
+            print("Access Denied: Your account is disabled.")
             return
 
-        self.logged_in_user = user  #Set logged-in user
-        self.bank.logged_in_user = user  #Also update Bank instance
-
-        # Refresh users list to include newly created accounts
-        self.bank.users = self.bank.get_accounts(self.accounts_file)  
-
-        # Pass self.bank to Accounts so it has updated data
-        account = Accounts(user, self.bank.users, self.accounts_file, self.bank, self)  
+        self.logged_in_user = user
+        account = Accounts(user, self.users, self.accounts_file, self.bank)  
         account.display_account_info()
+        self.logout()
 
-
+    
     def logout(self):
-
-        while True:
-            self.clear_screen()
-            print("Logging out...")
-            self.logged_in_user = None
-
-            #Clear new accounts so they can be accessed in the next session
-            self.newly_created_accounts.clear()
-
-            print("You have been logged out. Would you like to log back in or exit the application?\n")
-            print("[1] to login")
-            print("[0] to exit the application")
-            session_type = input("Enter choice (1 or 0): ").strip()
-
-            if session_type == "1":
-                self.login()
-            elif session_type == "0":
-                print("Exiting application...")
-                exit()
-            else:
-                print(Fore.RED + "Invalid selection. Please choose 1 or 0." + Style.RESET_ALL)
-
-
+        print("Logging out...")
+        self.logged_in_user = None
+        print("You have been logged out. Returning to login screen.\n")
+        self.login()
+    
     def clear_screen(self):
         os.system('cls' if os.name == 'nt' else 'clear')
+
